1f38729bf988ca98d8550a84f062196f1908021b
tasajifood-pixel <canaibumariam@gmail.com>
2026-01-08 09:33:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* sw.js - Tasaji POS PWA (safe starter) */
-const CACHE_VERSION = "tasaji-pos-v2026-01-08-01"; // ganti tiap release
+const CACHE_VERSION = "tasaji-pos-v2026-01-08-02"; // ganti tiap release
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -53,3 +53,4 @@ self.addEventListener("fetch", (event) => {
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
 });
+
